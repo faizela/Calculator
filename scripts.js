a8c852857eba9b5a9cf1f58607e4c9ce99080a6d
Faizel Ahmed <feisala132@gmail.com>
2024-03-19 14:09:39 +0000

    Adding btns

--- scripts.js
+++ scripts.js
@@ -2,26 +2,48 @@ function add(num_1, num_2) {
  return num_1 + num_2
 }
 
-console.log(add(2,2)) // 4
+
 
 function subtract(num_1, num_2) {
     return num_1 - num_2
 }
 
 
-console.log(subtract(4,2)) // 2
 
 
 function multiply(num_1, num_2) {
  return num_1 * num_2
 }
 
-console.log(multiply(3,3)) // 9
 
 
 function divide(num_1, num_2) {
     return num_1 / num_2
 }
 
-console.log(divide(10,2)) // 5
    
+let first_num
+let sec_num
+let operator
+
+
+// operator = '-'
+
+function operate(operator, first_num, sec_num) {
+ if (operator === '+') {
+    return add(first_num, sec_num)
+ }
+ else if (operator === '-') {
+    return subtract(first_num, sec_num)
+ }
+
+ else if (operator === '*') {
+    return multiply(first_num, sec_num)
+ }
+
+ else if (operator === '/') {
+    return divide(first_num, sec_num)
+ }
+}
+
+// console.log(operate(operator, 2, 2))
